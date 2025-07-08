0c320c94e228dd60f0729952dc8c353b60df8af7
CameronRobsonLeighBright <cameron.robson-leigh@brighthr.com>
2025-07-08 14:57:49 +0100

    refac and golden file

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -1,88 +1,78 @@
-﻿namespace GildedRoseKata;
-
-public class GildedRose
+﻿namespace GildedRoseKata
 {
-    IList<Item> Items;
-    public GildedRose(IList<Item> Items)
+    public class GildedRose
     {
-        this.Items = Items;
-    }
+        IList<Item> Items;
 
-    public void UpdateQuality()
-    {
-        for (var i = 0; i < Items.Count; i++)
+        public GildedRose(IList<Item> Items)
         {
-            var qualityChange = Items[i].Name.Contains("Conjured") ? 2 : 1;
+            this.Items = Items;
+        }
 
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
             {
-                if (Items[i].Quality > 0)
+                int qualityChange = item.Name.Contains("Conjured") ? 2 : 1;
+
+                switch (item.Name)
                 {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - qualityChange;
-                    }
+                    case string name when name.Contains("Aged Brie"):
+                        UpdateAgedBrie(item, qualityChange);
+                        break;
+
+                    case string name when name.Contains("Backstage passes"):
+                        UpdateBackstagePass(item, qualityChange);
+                        break;
+
+                    case string name when name.Contains("Sulfuras"):
+                        // Legendary item, no changes
+                        break;
+
+                    default:
+                        UpdateNormalItem(item, qualityChange);
+                        break;
                 }
             }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + qualityChange;
+        }
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + qualityChange;
-                            }
-                        }
+        private void UpdateAgedBrie(Item item, int qualityChange)
+        {
+            if (item.Quality < 50)
+                item.Quality += qualityChange;
 
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + qualityChange;
-                            }
-                        }
-                    }
-                }
-            }
+            item.SellIn--;
 
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
+            if (item.SellIn < 0 && item.Quality < 50)
+                item.Quality += qualityChange;
+        }
 
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - qualityChange;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + qualityChange;
-                    }
-                }
-            }
+        private void UpdateBackstagePass(Item item, int qualityChange)
+        {
+            if (item.Quality < 50)
+                item.Quality += qualityChange;
+
+            if (item.SellIn < 11 && item.Quality < 50)
+                item.Quality += qualityChange;
+
+            if (item.SellIn < 6 && item.Quality < 50)
+                item.Quality += qualityChange;
+
+            item.SellIn--;
+
+            if (item.SellIn < 0)
+                item.Quality = 0;
+        }
+
+        private void UpdateNormalItem(Item item, int qualityChange)
+        {
+            if (item.Quality > 0)
+                item.Quality -= qualityChange;
+
+            item.SellIn--;
+
+            if (item.SellIn < 0 && item.Quality > 0)
+                item.Quality -= qualityChange;
         }
     }
 }
